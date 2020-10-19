98c667f62eef84e8817f616802c5c7b93586eb8a
Scott Henderson <scottyh@uw.edu>
2020-10-18 17:38:20 -0700

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# greenland-ice-mapping-project
-Example notebooks for working with data from the greenland ice sheet mapping project
+# Greenland Ice Mapping Project
+Example notebooks for working with data from the [Greenland Ice Sheet Mapping Project](https://nsidc.org/data/measures/gimp), which provides benchmark data sets for observing ice sheet change and stability.
+
+These datasets cover the entire landmass of Greenland and consequently images can be quite large. Often researchers are just interested in data covering a specific glacier. *The goal of these notebooks is to illustrate how to efficiently work with data using modern Python libraries that can read imagery directly on a server without downloading entire files first.* 
+
+We'll work with Sentinel-1 backscatter mosasics stored as [Cloud-Optimized Geotiffs (COG)](https://nsidc.org/data/nsidc-0723). This imagery is publically-available and hosted by NASA's [National Snow and Ice Data Center](https://nsidc.org/data/nsidc-0723).
+
+
+## Run these notebooks on the Cloud:
+Clicking the following button will transport you into a temporary server running on [mybinder.org](https://mybinder.org/)
+
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/scottyhq/greenland-ice-mapping-project/master?urlpath=lab)
+
+
+## Run these notebooks on your own computer:
+This requires that you have [conda](https://docs.conda.io/en/latest/miniconda.html) installed.
+```
+git clone https://github.com/scottyhq/greenland-ice-mapping-project.git
+cd greenland-ice-mapping-project
+conda env create -f .binder/environment.yml
+conda activate greenland-ice-mapping
+jupyter lab
+```
---
+++ cmr.py
@@ -0,0 +1,87 @@
+'''
+Simplified version of NSIDC Download Script to return links without downloading
+'''
+
+import requests
+import itertools
+
+CMR_URL = 'https://cmr.earthdata.nasa.gov'
+URS_URL = 'https://urs.earthdata.nasa.gov'
+CMR_PAGE_SIZE = 2000
+CMR_FILE_URL = ('{0}/search/granules.json?provider=NSIDC_ECS'
+                '&sort_key[]=start_date&sort_key[]=producer_granule_id'
+                '&scroll=true&page_size={1}'.format(CMR_URL, CMR_PAGE_SIZE))
+
+def cmr_filter_urls(search_results):
+    """Select only the desired data files from CMR response."""
+    if 'feed' not in search_results or 'entry' not in search_results['feed']:
+        return []
+
+    entries = [e['links']
+               for e in search_results['feed']['entry']
+               if 'links' in e]
+    # Flatten "entries" to a simple list of links
+    links = list(itertools.chain(*entries))
+
+    urls = []
+    unique_filenames = set()
+    for link in links:
+        if 'href' not in link:
+            # Exclude links with nothing to download
+            continue
+        if 'inherited' in link and link['inherited'] is True:
+            # Why are we excluding these links?
+            continue
+        if 'rel' in link and 'data#' not in link['rel']:
+            # Exclude links which are not classified by CMR as "data" or "metadata"
+            continue
+
+        if 'title' in link and 'opendap' in link['title'].lower():
+            # Exclude OPeNDAP links--they are responsible for many duplicates
+            # This is a hack; when the metadata is updated to properly identify
+            # non-datapool links, we should be able to do this in a non-hack way
+            continue
+
+        filename = link['href'].split('/')[-1]
+        if filename in unique_filenames:
+            # Exclude links with duplicate filenames (they would overwrite)
+            continue
+        unique_filenames.add(filename)
+
+        urls.append(link['href'])
+
+    return urls
+
+
+def query_cmr(query_url):
+    ''' return JSON / python dictionary'''
+    print(query_url)
+    response = requests.get(query_url)
+    search_results = response.json()
+    return search_results
+
+
+
+def build_cmr_query_url(short_name, version, time_start, time_end,
+                        bounding_box=None, polygon=None,
+                        filename_filter=None):
+    params = '&short_name={0}'.format(short_name)
+    params += '&version={0}'.format(version)
+    params += '&temporal[]={0},{1}'.format(time_start, time_end)
+    if polygon:
+        params += '&polygon={0}'.format(polygon)
+    elif bounding_box:
+        params += '&bounding_box={0}'.format(bounding_box)
+    if filename_filter:
+        option = '&options[producer_granule_id][pattern]=true'
+        params += '&producer_granule_id[]={0}{1}'.format(filename_filter, option)
+    return CMR_FILE_URL + params
+
+
+
+def get_urls(short_name, version, time_start, time_end, bounding_box, polygon, filename_filter):
+    query_url = build_cmr_query_url(short_name, version, time_start, time_end, bounding_box, polygon, filename_filter)
+    search_results = query_cmr(query_url)
+    urls = cmr_filter_urls(search_results)
+    return urls
+    
